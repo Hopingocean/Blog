bceb76744b6ee2517594b89ee436ba93ce684757
Lee <1258963839@qq.com>
2018-01-04 19:37:12 +0800

    blog-2018/01/04

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+// 内容
+var Home = Vue.component('v-home', {
+  template: '#v-home-template',
+})
+
+// 关于我
+var About = Vue.component('v-about', {
+  template: '#v-about-template',
+})
+
+// 导航栏组件
+Vue.component('v-header', {
+  template: '#v-header-template',
+})
+
+// 底部组件
+Vue.component('v-footer', {
+  template: '#v-footer-template',
+})
+
+var routes = [
+  {path: '/home', component: Home},
+  {path: '/about', component: About}
+]
+
+var router = new VueRouter({
+  routes: routes
+})
+
+var app = new Vue({
+  el: '#app',
+  router: router,
+  data: {},
+  computed: {},
+  watch: {},
+  methods: {},
+  mounted: function () {
+    this.$nextTick(function () {
+
+    })
+  }
+})
