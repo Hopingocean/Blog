6d8638f394cbe049c155dc05c2614bcb21dc03b1
Lee <1258963839@qq.com>
2018-01-05 18:06:29 +0800

    201801/05

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
+// header
+var Header = Vue.component('v-header', {
+  template: '#v-header-template',
+})
 // 内容
+var homeBlogList = {homeBlogList: [
+  {
+    icon: 'http://oak1q2h54.bkt.clouddn.com/blog/0.jpg',
+    title: 'cloc代码统计工具',
+    desc: '项目地址：https://github.com/AlDanial/cloc',
+    date: '2017年04月18日'
+  }
+]};
 var Home = Vue.component('v-home', {
   template: '#v-home-template',
+  data: function () {
+    return homeBlogList;
+  }
+})
+
+// 分类
+var Category = Vue.component('v-category', {
+  template: '#v-category-template',
+})
+
+// 标签
+var Tag = Vue.component('v-tag', {
+  template: '#v-tag-template',
 })
 
 // 关于我
@@ -9,36 +34,43 @@ var About = Vue.component('v-about', {
 })
 
 // 导航栏组件
-Vue.component('v-header', {
-  template: '#v-header-template',
+var Sidebar = Vue.component('v-sidebar', {
+  template: '#v-sidebar-template',
 })
 
 // 底部组件
-Vue.component('v-footer', {
+var Footer = Vue.component('v-footer', {
   template: '#v-footer-template',
 })
 
 var routes = [
+  {path: '/', component: Home},
   {path: '/home', component: Home},
+  {path: '/header', component: Header},
+  {path: '/category', component: Category},
+  {path: '/tag', component: Tag},
   {path: '/about', component: About}
 ]
-
+// 初始化VueRouter
 var router = new VueRouter({
   routes: routes
 })
-
+// 初始化Vue
 var app = new Vue({
   el: '#app',
   router: router,
-  data: {
-    behavior: '1',
-  },
+  data: {},
   computed: {},
   watch: {},
   methods: {},
   mounted: function () {
     this.$nextTick(function () {
-
+      getHomeList();
     })
   }
 })
+
+// 获取首页博客列表
+function getHomeList() {
+
+}
