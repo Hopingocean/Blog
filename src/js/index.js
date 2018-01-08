2449b99054b7c0b561355987ffbd3f0004d513be
Lee <1258963839@qq.com>
2018-01-08 18:12:13 +0800

    2018/01/08

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,46 @@
+var bus = new Vue();
 // header
 var Header = Vue.component('v-header', {
   template: '#v-header-template',
-})
-// 内容
-var homeBlogList = {homeBlogList: [
-  {
-    icon: 'http://oak1q2h54.bkt.clouddn.com/blog/0.jpg',
-    title: 'cloc代码统计工具',
-    desc: '项目地址：https://github.com/AlDanial/cloc',
-    date: '2017年04月18日'
+  data: function () {
+    return {};
+  },
+  methods: {
+    showSidebar: function () {
+      bus.$emit('behaviorChanged');
+    }
   }
-]};
+})
+
+// 导航栏组件
+var Sidebar = Vue.component('v-sidebar', {
+  template: '#v-sidebar-template',
+  data: function () {
+    return {
+      behavior: 1,
+    };
+  },
+  created: function () {
+    bus.$on('behaviorChanged', function () {
+      this.behavior = 2;
+    })
+  },
+  mehtods: {
+    
+  }
+})
+
+// 内容
+var homeBlogList = {
+  homeBlogList: [
+    {
+      icon: 'http://oak1q2h54.bkt.clouddn.com/blog/0.jpg',
+      title: 'cloc代码统计工具',
+      desc: '项目地址：https://github.com/AlDanial/cloc',
+      date: '2017年04月18日'
+    }
+  ]
+};
 var Home = Vue.component('v-home', {
   template: '#v-home-template',
   data: function () {
@@ -33,11 +63,6 @@ var About = Vue.component('v-about', {
   template: '#v-about-template',
 })
 
-// 导航栏组件
-var Sidebar = Vue.component('v-sidebar', {
-  template: '#v-sidebar-template',
-})
-
 // 底部组件
 var Footer = Vue.component('v-footer', {
   template: '#v-footer-template',
@@ -51,18 +76,30 @@ var routes = [
   {path: '/tag', component: Tag},
   {path: '/about', component: About}
 ]
+
 // 初始化VueRouter
 var router = new VueRouter({
   routes: routes
 })
+
 // 初始化Vue
 var app = new Vue({
   el: '#app',
   router: router,
-  data: {},
+  data: {
+    behavior: 1,
+  },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    behaviorChange: function (i) {
+      this.behavior = i;
+    }
+  },
+  components: {
+    Header,
+    Sidebar,
+  },
   mounted: function () {
     this.$nextTick(function () {
       getHomeList();
