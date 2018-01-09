56ba46e445525138876d9052e1bcac9ba9c67ffa
Lee <1258963839@qq.com>
2018-01-09 11:41:10 +0800

    2018/01/09

--- src/js/index.js
+++ src/js/index.js
@@ -3,38 +3,38 @@ var bus = new Vue();
 var Header = Vue.component('v-header', {
   template: '#v-header-template',
   data: function () {
-    return {};
+    return {
+      behavior: 1,
+    };
   },
   methods: {
     showSidebar: function () {
-      bus.$emit('behaviorChanged');
+      this.behavior = (this.behavior == 1) ? 2 : 1;
+      this.$emit('tochangebehavior', this.behavior); // $emit传入的事件名称只能使用小写，不能使用大写的驼峰规则命名
     }
   }
 })
 
 // 导航栏组件
 var Sidebar = Vue.component('v-sidebar', {
+  props: ['behavior'],
   template: '#v-sidebar-template',
   data: function () {
-    return {
-      behavior: 1,
-    };
+    return { };
   },
   created: function () {
     bus.$on('behaviorChanged', function () {
       this.behavior = 2;
     })
   },
-  mehtods: {
-    
-  }
+  mehtods: {}
 })
 
 // 内容
 var homeBlogList = {
   homeBlogList: [
     {
-      icon: 'http://oak1q2h54.bkt.clouddn.com/blog/0.jpg',
+      icon: 'http://oak1q2h54.bkt.clouddn.com/blog/ljdzz_icon.png',
       title: 'cloc代码统计工具',
       desc: '项目地址：https://github.com/AlDanial/cloc',
       date: '2017年04月18日'
@@ -42,6 +42,7 @@ var homeBlogList = {
   ]
 };
 var Home = Vue.component('v-home', {
+  props: ['behavior'],
   template: '#v-home-template',
   data: function () {
     return homeBlogList;
@@ -92,7 +93,7 @@ var app = new Vue({
   computed: {},
   watch: {},
   methods: {
-    behaviorChange: function (i) {
+    changeBehavior: function (i) {
       this.behavior = i;
     }
   },
